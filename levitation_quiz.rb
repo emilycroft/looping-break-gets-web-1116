22538ed4d683b49bdfe52cc64c71fb883750e249
Emily Croft <emilyhcroft@gmail.com>
2016-10-27 13:41:24 +0000

    Done.

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,9 @@
 
 def levitation_quiz
-	#your code here
+	while true
+		puts "What is the spell that enacts levitation?"
+		answer = gets.strip
+		break if answer == "Wingardium Leviosa"
+	end
+	puts "You passed the quiz!"
 end
-
-
